de0c6f85721ec81430dd02f0b5c038602f026ef7
Adewale IyanuOluwa Isaac <60206492+Ultra-Tech-code@users.noreply.github.com>
2021-12-01 22:32:16 +0100

    First commit

---
+++ src/components/Tour.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Tour = ({contents, settour, tour}) => {
+  return (
+    <>
+      {contents.map((content) =>{
+        const {id, image, name, price, info} = content;
+
+        const deleteTour = (id) =>{
+          settour(tour.filter((tour) => tour.id !== id))
+        }
+
+
+        return(
+          <>
+          <section key={id}>
+            <main>
+              <h5 className="title underline">Our Tour</h5>
+              <div className="single-tour">
+              <img src={image} alt="tour_img" />
+              <div className="tour-info">
+                <h4>{name}</h4>
+                <p className="tour-price">{price}</p>
+              </div>
+              <footer> {info} 
+              <button className="delete-btn" onClick={() => deleteTour(id)}>Not interested</button> 
+              </footer>        
+              </div>  
+            </main> 
+          </section>  
+          </>
+
+        )
+      })
+      }
+           
+    </>
+  )
+}
+
+export default Tour
+
+
