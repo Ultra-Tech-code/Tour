b152d3f93977693299f5825a9ccc711a2e047ff2
Adewale IyanuOluwa Isaac <60206492+Ultra-Tech-code@users.noreply.github.com>
2021-12-01 22:18:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+//import Loading from './components/Loading';
+import Tour from './components/Tour';
+
+const url = 'https://course-api.com/react-tours-project';
 
 function App() {
+  const [tour, setTour] = useState([]);
+  const [Load, setLoad] = useState(false);
+  const [error, setError] = useState();
+
+  function getApiData(){ 
+    fetch(url)
+    .then((res) => {
+      //return res.json();
+      if(res.status >= 200 && res.status <= 299){
+          return res.json();
+      } else{
+        setError(res.statusText);
+      }
+    })
+    .then((data) => {
+      setTour(data);
+      setLoad(true);
+      }).catch((err) => {
+     setError(err);
+    })  
+  };  
+  useEffect(() => {
+    getApiData(); 
+  }, [])
+
+  if (error || !Array.isArray(tour)) {
+    return <p>There was an error loading your data!</p>;
+  }
+
+  // if(Load){
+  //   return <h2 className="loading">Loading...</h2>;
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {Load ? (<Tour  contents={tour} settour={setTour} tour={tour}/>) : <h2 className="loading">Loading...</h2>}
+ 
     </div>
   );
+
+  // if(tour.length <= 0){
+  //   return <div className="section">
+  //       <h3>No more Tours Remaining</h3>
+  //       <button className="btn">Refresh</button>;
+  //   </div>
+   
+
+  // }
+
 }
 
 export default App;
